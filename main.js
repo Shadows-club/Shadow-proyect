d13bf62de26a3544a71b8d33b1c7f3c75a52e1ad
𝘿𝙚𝙫-𝙛𝙚𝙙𝙚𝙭𝙮𝙯 🪐 <215192609+dev-fedexyzz@users.noreply.github.com>
2025-11-19 15:40:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
-import './config.js'
+import './shadow/config.js'
 import cluster from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts'
